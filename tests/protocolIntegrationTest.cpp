73d3782c1e7916c56bad6e0535a90495d09c31a4
nicorf12 <nromano@fi.uba.ar>
2025-05-17 15:56:41 -0300

    installer

--- tests/protocolIntegrationTest.cpp
+++ tests/protocolIntegrationTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
-#include "../common/protocol.h"
-#include "../common/socket.h"
+#include "../common/communication/protocol.h"
+#include "../common/communication/socket.h"
 
 
 TEST(ProtocolIntegrationTest, SendAndReceiveCreateMessage) {
