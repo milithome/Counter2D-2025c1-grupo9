9bf9cd0527a539e395a0b63e5d510e0f5defcbf4
nicorf12 <nromano@fi.uba.ar>
2025-05-19 16:12:05 -0300

    feat:refactor

--- server/clientHandler.cpp
+++ server/clientHandler.cpp
@@ -5,20 +5,20 @@ ClientHandler::ClientHandler(
     Admin& admin,
     std::function<void(std::string, std::shared_ptr<ClientHandler>)> onRegister)
     : protocol(std::move(protocol)),
-        clientName(""),
-        active(true),
-        admin(admin),
-        onRegister(std::move(onRegister)) {}
+      clientName(""),
+      active(true),
+      admin(admin),
+      onRegister(std::move(onRegister)) {}
 
 ClientHandler::ClientHandler(
     Protocol protocol,
     const std::string& clientName,
     Admin& admin)
     : protocol(std::move(protocol)),
-        clientName(clientName),
-        active(true),
-        admin(admin),
-        onRegister(nullptr) {}
+      clientName(clientName),
+      active(true),
+      admin(admin),
+      onRegister(nullptr) {}
 
 ClientHandler::~ClientHandler() {
     std::cout << "ClientHandler destructor called." << std::endl;
@@ -30,23 +30,10 @@ void ClientHandler::run() {
             clientName = protocol.recv_name();
             onRegister(clientName, shared_from_this());
         }
+
         while (active) {
             Message message = protocol.recv_message();
-
-            switch (message.type) {
-                case Type::CREATE:
-                    handle_create(message.name);
-                    handle_join(message.name);
-                    break;
-                case Type::JOIN:
-                    handle_join(message.name);
-                    break;
-                case Type::LIST:
-                    handle_list();
-                    break;
-                default:
-                    std::cerr << "Unknown message type received." << std::endl;
-            }
+            handle_message(message);
         }
 
         admin.removeHandler(clientName);
@@ -57,124 +44,133 @@ void ClientHandler::run() {
     }
 }
 
+void ClientHandler::handle_message(const Message& message) {
+    switch (message.type) {
+        case Type::CREATE:
+            handle_create(message.name);
+            handle_join(message.name);
+            break;
+        case Type::JOIN:
+            handle_join(message.name);
+            break;
+        case Type::LIST:
+            handle_list();
+            break;
+        case Type::DISCONNECT:
+            send_simple_response(Type::DISCONNECT, "Disconnect successful");
+            active = false;
+            break;
+        default:
+            std::cerr << "Unknown message type received." << std::endl;
+            break;
+    }
+}
+
 void ClientHandler::handle_create(const std::string& name) {
     try {
         admin.createLobby(name);
     } catch (const std::exception& e) {
-        std::cerr << "Error in create lobby" << std::endl;
+        std::cerr << "Error creating lobby: " << e.what() << std::endl;
     }
 }
 
 void ClientHandler::handle_join(const std::string& name) {
     try {
         LobbyChannels queues = admin.joinLobby(name, clientName, protocol);
-        Response response = {
-            Type::JOIN,
-            0,
-            {},
-            {},
-            0, // exito
-            ""
-        };
-        protocol.send_response(response);
+        send_simple_response(Type::JOIN, "Joined lobby");
 
-        Queue<LobbyEvent>& toLobby  = *queues.toLobby;
+        Queue<LobbyEvent>& toLobby = *queues.toLobby;
         Queue<LobbyEvent>& fromLobby = *queues.fromLobby;
-
 
         bool inLobby = true;
         while (inLobby) {
-            Response response;
             if (protocol.has_data()) {
                 Message msg = protocol.recv_message();
-                if (msg.type == Type::LEAVE) {
-                    toLobby.push({LobbyEventType::LEAVE, clientName});
-                    inLobby = false;
-                    response = {
-                            Type::LEAVE,
-                            0, {}, {}, 0, "Leave successfull"
-                        };
-                    protocol.send_response(response);
+                if (handle_lobby_client_message(msg, toLobby, inLobby)) {
                     continue;
                 }
             }
 
             LobbyEvent event;
-            if (fromLobby.try_pop(event)) {
-                switch (event.type) {
-                    case LobbyEventType::START:                        
-                        inLobby = false;
-                        handle_game(name);
-                        break;
-                    default:
-                        break;
-                }
+            if (fromLobby.try_pop(event) && event.type == LobbyEventType::START) {
+                inLobby = false;
+                handle_game(name);
             }
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     } catch (const std::exception& e) {
         std::cerr << "Error in lobby: " << e.what() << std::endl;
     }
 }
 
-void ClientHandler::handle_game(const std::string& name){
-    try {
-        Response response = {
-            Type::START,
-            0,
-            {},
-            {},
-            0, // exito
-            ""
-        };
-        protocol.send_response(response);
-        GameChannels queues = admin.joinGame(name, clientName, protocol);
-        Queue<ActionEvent>& toGame  = *queues.toGame;
-        Queue<ActionEvent>& fromGame = *queues.fromGame;
+bool ClientHandler::handle_lobby_client_message(const Message& msg, Queue<LobbyEvent>& toLobby, bool& inLobby) {
+    Response response;
+    switch (msg.type) {
+        case Type::LEAVE:
+            toLobby.push({LobbyEventType::LEAVE, clientName});
+            inLobby = false;
+            send_simple_response(Type::LEAVE, "Leave successful");
+            return true;
+        case Type::DISCONNECT:
+            active = false;
+            inLobby = false;
+            send_simple_response(Type::DISCONNECT, "Disconnect successful");
+            return true;
+        default:
+            break;
+    }
+    return false;
+}
 
+void ClientHandler::handle_game(const std::string& name) {
+    try {
+        send_simple_response(Type::START, "Game started");
+
+        GameChannels queues = admin.joinGame(name, clientName, protocol);
+        Queue<ActionEvent>& toGame = *queues.toGame;
+        Queue<ActionEvent>& fromGame = *queues.fromGame;
 
         bool inGame = true;
         while (inGame) {
             if (protocol.has_data()) {
                 Message msg = protocol.recv_message();
-
-                if (msg.type == Type::LEAVE) {
-                    inGame = false;
-                    break;
-                }
-
-                if (msg.type == Type::ACTION) {
-                    ActionEvent event;
-                    event.action = msg.action;
-                    event.playerName = clientName;
-                    toGame.push(std::move(event));
+                if (handle_game_client_message(msg, toGame, inGame)) {
+                    continue;
                 }
             }
 
-            ActionEvent outgoingEvent;
-            if (fromGame.try_pop(outgoingEvent)) {
-                switch (outgoingEvent.action.type) {
-                    case ActionType::FINISH:
-                        response = {
-                            Type::FINISH,
-                            0, {}, {}, 0, "Game finish"
-                        };
-                        protocol.send_response(response);
-                        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-                        inGame = false;
-                        break;
-                    default:
-                        break;
-                }
+            ActionEvent event;
+            if (fromGame.try_pop(event) && event.action.type == ActionType::FINISH) {
+                send_simple_response(Type::FINISH, "Game finished");
+                inGame = false;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     } catch (const std::exception& e) {
         std::cerr << "Exception in Game: " << e.what() << std::endl;
     } catch (...) {
-        std::cerr << "Unknown exception in Game: " << std::endl;
+        std::cerr << "Unknown exception in Game." << std::endl;
     }
+}
+
+bool ClientHandler::handle_game_client_message(const Message& msg, Queue<ActionEvent>& toGame, bool& inGame) {
+    Response response;
+    switch (msg.type) {
+        case Type::LEAVE:
+            inGame = false;
+            return true;
+        case Type::ACTION: {
+            ActionEvent event{msg.action, clientName};
+            toGame.push(std::move(event));
+            return false;
+        }
+        case Type::DISCONNECT:
+            active = false;
+            inGame = false;
+            send_simple_response(Type::DISCONNECT, "Disconnect successful");
+            return true;
+        default:
+            break;
+    }
+    return false;
 }
 
 void ClientHandler::handle_list() {
@@ -185,7 +181,7 @@ void ClientHandler::handle_list() {
             static_cast<uint16_t>(lobbies.size()),
             {},
             lobbies,
-            0,  // 0 para éxito
+            0,
             ""
         };
         protocol.send_response(response);
@@ -193,14 +189,26 @@ void ClientHandler::handle_list() {
         std::cerr << "Error listing lobbies: " << e.what() << std::endl;
         Response response = {
             Type::LIST,
-            0,  // Tamaño 0 en caso de error
+            0,
             {},
             {},
-            1,  // 1 para error
+            1,
             "Error listing lobbies"
         };
         protocol.send_response(response);
     }
+}
+
+void ClientHandler::send_simple_response(Type type, const std::string& msg) {
+    Response response = {
+        type,
+        0,
+        {},
+        {},
+        0,
+        msg
+    };
+    protocol.send_response(response);
 }
 
 void ClientHandler::stop() {
--- server/clientHandler.h
+++ server/clientHandler.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <functional>
+#include <memory>
 #include "../common/communication/protocol.h"
 #include "../common/structures.h"
 #include "admin.h"
@@ -10,14 +11,16 @@
 
 class ClientHandler : public Thread, public std::enable_shared_from_this<ClientHandler> {
 public:
-    explicit ClientHandler(Protocol protocol, Admin& admin, std::function<void(std::string, std::shared_ptr<ClientHandler>)> onRegister);
-    ClientHandler(Protocol protocol, const std::string& clientName, Admin& admin);
+    ClientHandler(Protocol protocol, Admin& admin,
+                  std::function<void(std::string, std::shared_ptr<ClientHandler>)> onRegister);
 
-    void run() override;
+    ClientHandler(Protocol protocol, const std::string& clientName, Admin& admin);
 
     ~ClientHandler() override;
 
+    void run() override;
     void stop() override;
+
 private:
     Protocol protocol;
     std::string clientName;
@@ -25,10 +28,21 @@ private:
     Admin& admin;
     std::function<void(std::string, std::shared_ptr<ClientHandler>)> onRegister;
 
+    // Main message handler
+    void handle_message(const Message& message);
+
+    // Handlers for message types
     void handle_create(const std::string& name);
     void handle_join(const std::string& name);
     void handle_list();
     void handle_game(const std::string& name);
+
+    // Lobby and game loops
+    bool handle_lobby_client_message(const Message& msg, Queue<LobbyEvent>& toLobby, bool& inLobby);
+    bool handle_game_client_message(const Message& msg, Queue<ActionEvent>& toGame, bool& inGame);
+
+    // Utility
+    void send_simple_response(Type type, const std::string& msg);
 };
 
 #endif
