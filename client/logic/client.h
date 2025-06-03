8694ff9242f0a0c56aa8ec8a6e81ab67dd6a75ca
Manuel <mcastagnari@fi.uba.ar>
2025-06-02 23:16:24 -0300

    comentarios con ideas para la modularizacion de cliente

--- client/logic/client.h
+++ client/logic/client.h
@@ -33,19 +33,32 @@ using namespace SDL2pp;
 #include <atomic>
 #include <memory>
 
+// Se pueden hacer 2 nuevas clases, una para el manejo de Qt y otra de SDL y dejar esta para el manejo de la inicializacion de ambas,
+// entre otras cosas
 class Client {
 private:
+    // Recursos que usa la parte tanto de Qt como SDL
+    // Estos podrian ser construidos en esta clase y pasados por referencia
+    // a la clase q se encarga de Qt y la clase q se encarga de SDL
     std::string clientName;
     std::unique_ptr<RecvLoop> receiver;
     std::unique_ptr<SendLoop> sender;
-    bool started;
 
-    std::vector<std::string> players;
-	QPoint w_pos_when_started;
+
+    // Atributos de transicion de Qt a SDL. Serian atributos de la clase de Qt, o tal vez podrian ser accedidos de alguna forma
+    // al finalizar la ejecucion de Qt, asi no son atributos de la clase
+    bool started;
+    QPoint w_pos_when_started;
+    std::vector<std::string> players;  // Raro, SDL la usa para inicializar el juego
+    // pero me pareceria raro q fuera un atributo de la clase de Qt
+
+    // Exclusivo Qt
     std::unique_ptr<QtWindow> menuWindow;
     QTimer* timer;
     std::unique_ptr<MenuController> menuController;
+    // aca seria logico q esta la QApplication tambien
 
+    // Exclusivo SDL
     std::unique_ptr<GameView> gameView;
     std::unique_ptr<GameController> gameController;
 
