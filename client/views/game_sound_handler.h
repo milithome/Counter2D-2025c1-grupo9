ea91780545f35da4a1d65fe368f2fb372ffdbaa8
Manuel <mcastagnari@fi.uba.ar>
2025-06-04 13:26:31 -0300

    Implementacion de playShotSound

--- client/views/game_sound_handler.h
+++ client/views/game_sound_handler.h
@@ -19,9 +19,32 @@ class GameSoundHandler {
         
         void playDeathSound(float distance);
 
+        void playMusic();
+
+        void mute() {
+            mixer.SetVolume(-1, 0); 
+        }
+
+        void unmute() {
+            mixer.SetVolume(-1, MIX_MAX_VOLUME); 
+        }
+
     private:
         Mixer mixer = Mixer(44100, MIX_DEFAULT_FORMAT, 2, 2048);
         int soundsPlaying = 0;
-}
+
+
+        Chunk glockSound = Chunk("../assets/sfx/weapons/glock18.wav");
+        Chunk awpSound = Chunk("../assets/sfx/weapons/awp.wav");
+        Chunk akSound = Chunk("../assets/sfx/weapons/ak47.wav");
+        Chunk m3Sound = Chunk("../assets/sfx/weapons/m3.wav");
+        Chunk knifeSound = Chunk("../assets/sfx/weapons/knife_slash.wav");
+        Chunk knifeHit = Chunk("../assets/sfx/weapons/knife_hit.wav");
+
+        Chunk bombSound = Chunk("../assets/sfx/weapons/c4_explode.wav");
+
+        Chunk bombSound = Chunk("../assets/sfx/weapons/c4_explode.wav");
+
+};
 
 #endif
